f83d87babc170df41615525d26f1a2667fde839a
mdbarnesUCSD <mdbarnes@ucsd.edu>
2024-03-21 14:04:47 -0700

    v1.1.6: Resolve handling of 288 context file path

--- setup.py
+++ setup.py
@@ -3,45 +3,57 @@ import os
 import shutil
 
 
-#remove the dist folder first if exists
+# remove the dist folder first if exists
 if os.path.exists("dist"):
-	shutil.rmtree("dist")
+    shutil.rmtree("dist")
+
 
 def readme():
-	this_directory = os.path.abspath(os.path.dirname(__file__))
-	with open(os.path.join(this_directory, 'README.md'), encoding='latin-1') as f:
-		long_description = f.read()
-		return(long_description)
-		
-VERSION = '1.1.5'
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(this_directory, "README.md"), encoding="latin-1") as f:
+        long_description = f.read()
+        return long_description
 
-def write_version_py(filename='SigProfilerSimulator/version.py'):
-	# Copied from numpy setup.py
-	cnt = """
+
+VERSION = "1.1.6"
+
+
+def write_version_py(filename="SigProfilerSimulator/version.py"):
+    # Copied from numpy setup.py
+    cnt = """
 # THIS FILE IS GENERATED FROM SIGPROFILERSIMULATOR SETUP.PY
 short_version = '%(version)s'
 version = '%(version)s'
 	
 	"""
-	fh = open(filename, 'w')
-	fh.write(cnt % {'version': VERSION,})
-	fh.close()
+    fh = open(filename, "w")
+    fh.write(
+        cnt
+        % {
+            "version": VERSION,
+        }
+    )
+    fh.close()
+
 
 write_version_py()
 
-setup(name='SigProfilerSimulator',
-		version=VERSION,
-		description='SigProfiler simulator tool',
-		long_description= readme(),
-		long_description_content_type='text/markdown',
-		url='',
-		author='Erik Bergstrom',
-		author_email='ebergstr@eng.ucsd.edu',
-		license='UCSD',
-		packages=find_packages(),
-		install_requires =[
-			"SigProfilerMatrixGenerator>=1.2.18",
-			"sigProfilerPlotting>=1.3.16",
-			"fastrand>=1.2"],
-		include_package_data=True,
-		zip_safe=False)
+setup(
+    name="SigProfilerSimulator",
+    version=VERSION,
+    description="SigProfiler simulator tool",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="Erik Bergstrom",
+    author_email="ebergstr@eng.ucsd.edu",
+    license="UCSD",
+    packages=find_packages(),
+    install_requires=[
+        "SigProfilerMatrixGenerator>=1.2.25",
+        "sigProfilerPlotting>=1.3.21",
+        "fastrand>=1.2",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
