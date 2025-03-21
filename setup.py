8f13ec558b35904edf111347f2f79ace8a7fc23b
mdbarnesUCSD <mdbarnes@ucsd.edu>
2025-03-21 13:39:12 -0700

    v1.2.1: Add support for mm39

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
         return long_description
 
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 
 
 def write_version_py(filename="SigProfilerSimulator/version.py"):
@@ -24,7 +24,7 @@ def write_version_py(filename="SigProfilerSimulator/version.py"):
 # THIS FILE IS GENERATED FROM SIGPROFILERSIMULATOR SETUP.PY
 short_version = '%(version)s'
 version = '%(version)s'
-Update = 'v1.2.0: Require Pandas and Numpy >= 2.0.0 and Python >= 3.9'
+Update = 'v1.2.1: Add support for mm39'
 	
 	"""
     fh = open(filename, "w")
@@ -52,7 +52,7 @@ setup(
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
-        "SigProfilerMatrixGenerator>=1.3.0",
+        "SigProfilerMatrixGenerator>=1.3.2",
         "sigProfilerPlotting>=1.4.0",
         "fastrand>=1.2",
     ],
