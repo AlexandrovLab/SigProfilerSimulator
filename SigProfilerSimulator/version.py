9736dc1c5dcb969ad92286f69aad389a2e0fb9a6
MousumyCSE <moukunducse@gmail.com>
2025-04-11 09:50:12 -0700

    Black formatting

--- SigProfilerSimulator/version.py
+++ SigProfilerSimulator/version.py
@@ -1,7 +1,4 @@
-
 # THIS FILE IS GENERATED FROM SIGPROFILERSIMULATOR SETUP.PY
-short_version = '1.2.1'
-version = '1.2.1'
-Update = 'v1.2.1: Add support for mm39'
-	
-	
+short_version = "1.2.1"
+version = "1.2.1"
+Update = "v1.2.1: Add support for mm39"
