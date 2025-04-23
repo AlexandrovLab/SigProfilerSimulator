3ea6e659ea7e15d1854e543ce28f21b6f48be6fa
MousumyCSE <moukunducse@gmail.com>
2025-04-23 12:19:02 -0700

    Added test to reproduce the result for same seeds

--- SigProfilerSimulator/version.py
+++ SigProfilerSimulator/version.py
@@ -1,4 +1,7 @@
+
 # THIS FILE IS GENERATED FROM SIGPROFILERSIMULATOR SETUP.PY
-short_version = "1.2.1"
-version = "1.2.1"
-Update = "v1.2.1: Add support for mm39"
+short_version = '1.2.1'
+version = '1.2.1'
+Update = 'v1.2.1: Add support for mm39'
+	
+	
